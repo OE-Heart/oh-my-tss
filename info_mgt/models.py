9b29c5c6256ae13adb45079778e41263df42cbad
RalXYZ <RalXYZ@pm.me>
2021-05-25 20:47:46 +0800

    add course table

--- info_mgt/models.py
+++ info_mgt/models.py
@@ -2,20 +2,28 @@ from django.db import models
 
 
 class User(models.Model):
+    STUDENT = 'S'
+    TEACHER = 'T'
+    ADMIN = 'A'
+    USER_ROLE_CHOICES = [
+        (STUDENT, 'Student'),
+        (TEACHER, 'Teacher'),
+        (ADMIN, 'Admin'),
+    ]
+
     zju_id = models.CharField(max_length=10)
     name = models.CharField(max_length=20)
-    role = models.CharField(max_length=1)
+    role = models.CharField(max_length=1, choices=USER_ROLE_CHOICES, default=STUDENT)
     hashed_password = models.CharField(max_length=30)
     token = models.CharField(max_length=30)
     last_login = models.DateTimeField()
+    # avatar = models.ImageField()
 
-    # TODO: add user avatar
 
-
-'''
 class Course(models.Model):
-    name
-    profession = models.CharField()  # 
-'''
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+    credit = models.FloatField()
+    major = models.CharField(max_length=15)
+    capacity = models.IntegerField()
 
-# Create your models here.
