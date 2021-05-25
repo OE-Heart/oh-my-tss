a0b0d27336052156cd38fbebd6a3394e5e8b9f48
RalXYZ <RalXYZ@pm.me>
2021-05-25 15:59:10 +0800

    add user table

--- info_mgt/models.py
+++ info_mgt/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
+class User(models.Model):
+    zju_id = models.CharField(max_length=10)
+    name = models.CharField(max_length=20)
+    role = models.CharField(max_length=1)
+    hashed_password = models.CharField(max_length=30)
+    token = models.CharField(max_length=30)
+    last_login = models.DateTimeField()
+
+    # TODO: add user avatar
+
+
+'''
+class Course(models.Model):
+    name
+    profession = models.CharField()  # 
+'''
+
 # Create your models here.
