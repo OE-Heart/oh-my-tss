4f356cbc1878005384dbe7d2224053026b7e9e80
RalXYZ <RalXYZ@pm.me>
2021-05-25 14:56:23 +0800

    init commit

---
+++ oh_my_tss/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for oh_my_tss project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oh_my_tss.settings')
+
+application = get_asgi_application()
---
+++ oh_my_tss/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for oh_my_tss project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oh_my_tss.settings')
+
+application = get_wsgi_application()
