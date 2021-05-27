061e9df332281aabe53eceaba921f9df948f03af
enzymii <enzymiix@gmail.com>
2021-05-27 22:11:43 +0800

    add basic element and style for skeleton

--- info_mgt/views.py
+++ info_mgt/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(req):
-  return render(req, 'info_mgt.html', {'test_param': 'TEST PARAM'})
+    return render(req, 'info_mgt.html', {
+        'test_param': 'TEST PARAM',
+        'nav_list': [
+            {'name': 'A', 'path': '/a'},
+            {'name': 'B', 'path': '/b'}
+        ]
+    })
--- oh_my_tss/urls.py
+++ oh_my_tss/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('skeleton/', include('info_mgt.urls'))
+    path('info_mgt/', include('info_mgt.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
