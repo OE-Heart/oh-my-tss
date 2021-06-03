d385fb3ad8f7b2f5f0dfcdebeeb8e6b578adb966
enzymii <enzymiix@gmail.com>
2021-06-04 00:32:28 +0800

    finish prototype of account list

--- info_mgt/urls.py
+++ info_mgt/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     # path('', generic.RedirectView.as_view(url='./main'), name="index"),
     path('', views.index, name="index"),
+    path('account', views.account, name="account"),
 ]
--- info_mgt/views.py
+++ info_mgt/views.py
@@ -6,8 +6,16 @@ from info_mgt.forms import SignupForm
 
 def index(req):
     return render(req, 'info_mgt.html', {
-        'web_title': '信息管理系统',
-        'page_title': '信息管理子系统',
+        'web_title': '信息管理',
+        'page_title': '信息管理',
         'test_param': 'TEST PARAM',
         'form': SignupForm
+    })
+
+def account(req):
+    return render(req, 'info_mgt.html', {
+        'web_title': '信息管理',
+        'page_title': '账户信息管理',
+        'test_param': 'asdfea',
+        'cur_submodule': 'account'
     })
