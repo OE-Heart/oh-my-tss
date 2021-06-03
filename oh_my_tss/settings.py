146eece6376009ff286eeb8b67820cb8213d669b
enzymii <enzymiix@gmail.com>
2021-06-03 18:54:26 +0800

    modify style

--- oh_my_tss/settings.py
+++ oh_my_tss/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'info_mgt',
-    'material.theme.amber',
+    'material.theme.lightblue',
     'material.frontend',
     'material',
 ]
