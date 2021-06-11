b87acd69bd72b96de8093d8703d581d0baeed9bb
Cyphantasm <wendy_grignard@163.com>
2021-06-11 21:28:29 +0800

    add table definitions & view prototypes & urls for class_schedule

--- class_schedule/models.py
+++ class_schedule/models.py
@@ -1,3 +1,45 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from info_mgt.models import Campus, Class, Teacher  # ,Admin
+import datetime
 
-# Create your models here.
+
+class Building(models.Model):
+    campus_id = models.ForeignKey(Campus, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+
+
+class Classroom(models.Model):
+    COMMON_ROOM = 'O'
+    GYM_ROOM = 'G'
+    COMPUTER_CENTER_ROOM = 'C'
+    TYPE_CHOICES = [
+        (COMMON_ROOM, 'Common Classroom'),
+        (GYM_ROOM, 'Gymnasium'),
+        (COMPUTER_CENTER_ROOM, 'Computer Room'),
+    ]
+    building_id = models.ForeignKey(Building, on_delete=models.CASCADE)
+    room_number = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    capacity = models.IntegerField(default=100, validators=[MinValueValidator(1)])
+    type = models.CharField(max_length=1, choices=TYPE_CHOICES, default=COMMON_ROOM)
+
+
+class ClassHasRoom(models.Model):
+    class_id = models.ForeignKey(Class, on_delete=models.CASCADE)
+    classroom_id = models.ForeignKey(Classroom, on_delete=models.SET_NULL, null=True)
+    day = models.IntegerField(default=1, null=True, blank=True, validators=[MaxValueValidator(7), MinValueValidator(1)])
+    start_at = models.IntegerField(default=1, null=True, blank=True, validators=[MinValueValidator(1), MaxValueValidator(13)])
+    # 按理说应该限制2节及以上长度的时段只能在1，3，6，7，9，11节开始，只有长度为1节的才是可取1~13，从view里定义限制吧……
+    duration = models.IntegerField(default=2, validators=[MinValueValidator(1), MaxValueValidator(4)])
+    # 这个最大值4哪来的？万恶的沟通技巧。
+
+
+class Application(models.Model):
+    submit_time = models.DateTimeField(default=datetime.datetime.now)
+    teacher_id = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    class_id = models.ForeignKey(Class, on_delete=models.CASCADE)
+    content = models.TextField(max_length=100)
+    reply_time = models.DateTimeField(null=True, blank=True)
+    # admin_id = models.ForeignKey(Admin, on_delete=models.SET_NULL)
+    is_accepted = models.BooleanField(null=True, blank=True)
+    reply = models.TextField(max_length=100, null=True, blank=True)
--- class_schedule/urls.py
+++ class_schedule/urls.py
@@ -4,4 +4,19 @@ from . import views
 
 urlpatterns = [
     # path('', generic.RedirectView.as_view(url='./main'), name='index'),
+    path('', views.index, name='index'),
+    path('add_room/', views.add_room, name='add_room'),
+    path('add_room_submit/', views.add_room_submit, name='add_room_submit'),
+    path('modify_room/', views.modify_room, name='modify_room'),
+    path('modify_room_submit/<int:room_id>', views.modify_room_submit, name='modify_room_submit'),
+    path('auto_schedule/', views.auto_schedule, name='auto_schedule'),
+    path('do_auto_schedule/', views.do_auto_schedule, name='do_auto_schedule'),
+    path('application/', views.application, name='application'),
+    path('submit_application', views.submit_application, name='submit_application'),
+    path('handle_application/', views.handle_application, name='handle_application'),
+    path('submit_handle/<int:application_id>', views.submit_handle, name='submit_handle'),
+    path('manipulate_schedule/', views.manipulate_schedule, name='manipulate_schedule'),
+    path('submit_manipulate/<int:class_has_room_id>', views.submit_manipulate, name='manipulate_submit'),
+    path('teacher_class/', views.teacher_class, name='teacher_class'),
+    path('room_class/', views.room_class, name='room_class'),
 ]
