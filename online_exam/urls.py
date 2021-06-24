805eea67f3fa80a399564c2171f0c8e7e5dad7c8
zpp987654321 <819858663@qq.com>
2021-06-24 21:43:27 +0800

    online_exam version3.0

--- online_exam/urls.py
+++ online_exam/urls.py
@@ -7,9 +7,16 @@ urlpatterns = [
     path('del_question/',views.del_question),
     path('generate_paper/', views.generate_paper),
     path('search_out/', views.searchout),
+    path('add_question/', views.add_question),
+    path('search_out/add_question/', views.add_question),
     path('release_test/', views.release),
     path('test_info/', views.testinfo, name="test_info"),
-    path('stu_analysis/', views.stuanalysis, name="stu_analysis"),
-    path('paper_ana/', views.paperanalysis, name="paper_ana")
+    path('generate_paper/auto/',views.generate_paper_auto),
+    path('generate_paper_auto_end/',views.generate_paper_auto),
+    path('generate_paper/handle/',views.generate_paper_handle),
+    path('generate_paper/handle_end/',views.generate_paper_handle_end),
+    path('combine_analysis/', views.combineanalysis, name="combine_analysis"),
+    path('stu_single/', views.stusingle, name="stu_single"),
+    path('stu_single_select/', views.stusingleselect, name="stu_single_select")
 ]
 
