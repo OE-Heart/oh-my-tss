0cdf59d458563e8574712da229357648a4cdb969
AXEwiges <2495477644@qq.com>
2021-06-26 17:57:07 +0800

    修复了表单检验的bug

--- info_mgt/views.py
+++ info_mgt/views.py
@@ -58,6 +58,8 @@ def info_view_with_username(req, username):
 
 def info_edit(req):
     def is_valid(req):
+        flag1 = False
+        flag2 = False
         try:
             flag1 = True
             query = models.User.objects.get(username=req.POST['username'])
@@ -134,6 +136,7 @@ def info_edit(req):
 
 def account_edit(req, username='#'):
     def is_valid(req):
+        flag1 = False
         flag2 = False
         try:
             flag1 = True
@@ -247,6 +250,9 @@ def account_edit(req, username='#'):
 
 def account_add(req):
     def is_valid(req):
+        flag1 = False
+        flag2 = False
+        flag3 = False
         try:
             flag1 = True
             query = models.User.objects.get(username=req.POST['username'])
